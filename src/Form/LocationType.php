cc758f72bb0b8a5dedb6335fabe95cf1c90fd8ec
pwiniarski85 <182wilder@gmail.com>
2024-10-18 19:52:53 +0200

    lab-d

---
+++ src/Form/LocationType.php
@@ -0,0 +1,45 @@
+<?php
+namespace App\Form;
+use App\Entity\Location;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+class LocationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('city', null, [
+            'attr' => [
+                'placeholder' => 'Enter city name',
+            ],
+        ])
+            ->add('country', ChoiceType::class, [
+                'choices' => [
+                    'Poland' => 'PL',
+                    'Germany' => 'GR',
+                    'France' => 'FR',
+                    'Spain' => 'ES',
+                    'Italy' => 'IT',
+                    'United Kingdom' => 'UK',
+                    'United States' => 'US',
+                ],
+            ])
+            ->add('latitude', null, [
+                'attr' => [
+                    'placeholder' => 'Enter latitude',
+                ],
+            ])
+            ->add('longitude', null, [
+                'attr' => [
+                    'placeholder' => 'Enter longitude',
+                ],
+            ]);
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Location::class,
+        ]);
+    }
+}
